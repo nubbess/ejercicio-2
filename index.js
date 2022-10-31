2534e6e244a9c50f69d890cbfdfaaec74d9d7e4f
nubbess <julianms999@gmail.com>
2022-10-31 01:45:31 -0300

    agregado
    le puse el punto y coma que me habia olvidado

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log("Hola Mundo!");
 console.log("soy el primer script!");
 let msg = String("Hola Mundo!\n Que facil es incluir \'comillas simples\'\n y \"comillas dobles\"");
-alert(msg)
+alert(msg);
